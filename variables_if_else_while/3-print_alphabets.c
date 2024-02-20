a34f2bebae7770a795129e3eff6584a3c689749b
my-holber <8762@holbertonstudents.com>
2024-02-21 00:44:23 +0100

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <='z'; abc++)
+	{
+	    putchar(abc);
+	}
+	for (abc = 'A'; abc <= 'Z'; abc++){
+	    putchar(abc);
+	}
+    putchar('\n');
+
+	return (0);
+}
