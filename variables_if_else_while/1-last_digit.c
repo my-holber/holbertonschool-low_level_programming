2bcb321e51e19249c7a956af3b7aff675bc75e2a
my-holber <8762@holbertonstudents.com>
2024-02-20 23:21:01 +0100

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,22 +8,21 @@
  */
 int main(void)
 {
-    int n;
-
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    int dernierChiffre = (n) % 10;
-    if (n > 5)
-    {
-        printf("the string  %dand%d is greater than 5\n", n, dernierChiffre);
-    }
-    else if (n == 0)
-    {
-        printf("the string %dand%d is 0", n, dernierChiffre);
-    }
-    else if (n < 6 && n != 0)
-    {
-        printf(" the string %dand%d is less than 6 and not 0\n", n, dernierChiffre);
-    }
-    return (0);
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int dernierChiffre = (n) % 10;
+	if (n > 5)
+	{
+		printf("the string  %dand%d is greater than 5\n", n, dernierChiffre);
+	}
+	else if (n == 0)
+	{
+		printf("the string %dand%d is 0", n, dernierChiffre);
+	}
+	else if (n < 6 && n != 0)
+	{
+		printf(" the string %dand%d is less than 6 and not 0\n", n, dernierChiffre);
+	}
+	return (0);
 }
