cdcc8bc88b08750faed166b8058a7b63da0da170
my-holber <8762@holbertonstudents.com>
2024-02-21 04:07:57 +0100

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
-  * main - Prints some letters of alphabet
+  * main - Prints the numbers since 0 to 9
   *
   * Return: Always (Success)
   */
@@ -11,9 +12,9 @@ int main(void)
 	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-
 	}
-	putchar('\n');
-	return (0)
 
+	putchar('\n');
+
+	return (0);
 }
