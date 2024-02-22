808b1ae83eff4d515274910c335e4346953de80c
my-holber <8762@holbertonstudents.com>
2024-02-22 04:13:17 +0100

    7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Prints the alphabet at reverse
- *
- * Return: Always (Success)
- */
+  * main - Prints the alphabet at reverse
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
 	char abc;
+
 	for (abc = 'z'; abc >= 'a'; abc--)
 	{
 		putchar(abc);
 	}
+
 	putchar('\n');
-	return 0;
+
+	return (0);
 }
