1d71d0c1287787b122c3784a9c5db3b03fa86242
my-holber <8762@holbertonstudents.com>
2024-02-23 11:14:24 +0100

    main.h

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
-  * print_alphabet - Make the alphabet
-  *
-  * Return: void
-  */
-void print_alphabet(void){
+* print_alphabet - Make the alphabet
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
 
 	char i;
-	for (i = 'a'; i <= 'z'; i++){
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
 	}
 	_putchar('\n');
