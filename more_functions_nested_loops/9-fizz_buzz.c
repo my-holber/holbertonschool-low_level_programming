6936ebce7413f53fb376eb5aaac1bb5da0a8310a
my-holber <8762@holbertonstudents.com>
2024-02-28 10:09:18 +0100

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,25 @@
- #include <stdio.h>
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: void.
+ */
 int main(void)
 {
-	for (int i = 1; i<=100; i++)
-	{
-		if ( i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if( i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if(i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", i);
-		}
-		if(i != 100)
-			printf(" ");
+	int i;
 
+	for (i = 1; i < 101; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) != 0))
+			printf("Fizz");
+		else if (((i % 5) == 0)  && ((i % 3) != 0))
+			printf("Buzz");
+		else if (((i % 5) == 0) && ((i % 3) == 0))
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
-
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
