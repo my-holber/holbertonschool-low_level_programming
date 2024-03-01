87251cfb8081e88776796e9ff69c36952aec575d
my-holber <8762@holbertonstudents.com>
2024-03-01 12:44:48 +0100

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
-
+	{
 		_putchar(str[i]);
-
+	}
 	_putchar('\n');
 }
