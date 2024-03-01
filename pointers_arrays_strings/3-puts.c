fb1144dc9a30b7956e17ca50e52ccc8e0a9c2e07
my-holber <8762@holbertonstudents.com>
2024-03-01 12:43:08 +0100

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
