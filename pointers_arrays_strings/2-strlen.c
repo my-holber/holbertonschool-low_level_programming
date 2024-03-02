b4eb5e6c2f144f94876e65e39a53bdc143fcfc9f
my-holber <8762@holbertonstudents.com>
2024-03-02 14:24:18 +0100

    2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
@@ -7,10 +8,14 @@
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	return (i);
 }
