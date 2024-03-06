ecac08f7b16ed090a9a809cf7e1aa5da76fdbb68
my-holber <8762@holbertonstudents.com>
2024-03-06 08:30:57 +0100

    8-print_diagsums.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 
 char *_strpbrk(char *s, char *accept);
 
-
 char *_strcpy(char *dest, char *src);
 
 void print_array(int *a, int n);
@@ -39,3 +38,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
