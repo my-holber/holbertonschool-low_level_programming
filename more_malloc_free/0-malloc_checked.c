f5a42b3db6c4e4d9ea803c7ed0d1d7c9f3be5d61
my-holber <8762@holbertonstudents.com>
2024-03-14 15:12:11 +0100

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
+ */
 
 void *malloc_checked(unsigned int b)
 {
