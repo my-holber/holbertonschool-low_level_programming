eb100a9012eaac34dbeef237ef8c43660057097c
my-holber <8762@holbertonstudents.com>
2024-03-15 12:57:05 +0100

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
+
+#endif
