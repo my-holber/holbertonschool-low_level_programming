e765efc00d929fffc9c9caad9aa7b64a9180a19b
my-holber <8762@holbertonstudents.com>
2024-03-20 13:45:46 +0100

    0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - вычисляет сумму переменного числа аргументов.
+ * @n: количество аргументов
+ * Вернет: сумму аргументов.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+	if (n!=0)
+	{
+		va_start(args, n);
+
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(args, int);
+
+		}
+		va_end(args);
+	}
+
+
+	return sum;
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef FUNC_VARIADIC
+#define FUNC_VARIADIC
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
