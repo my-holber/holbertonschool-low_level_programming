d2d118389a2ebfb8a6e5ea6b7b87657f305a6d7a
my-holber <8762@holbertonstudents.com>
2024-04-09 11:21:19 +0200

    Add _strncat function to concatenate two strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: Maximum number of characters to concatenate from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+		ptr++;
+
+	while (*src != '\0' && n > 0)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+		n--;
+	}
+
+	*ptr = '\0';
+	return dest;
+}
